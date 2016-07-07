c3a2b23f011d2682a5ffb2a060bdb2b66ea398e7
Thorsten Westheider <thorsten.westheider@outlook.com>
2016-07-07 18:47:13 +0200

    Karma configuration

--- config/karma-test-shim.js
+++ config/karma-test-shim.js
@@ -1,5 +1,3 @@
-// Test shim for Karma, needed to load files via SystemJS
-
 /*global jasmine, __karma__, window*/
 Error.stackTraceLimit = Infinity;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
@@ -7,45 +5,81 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
 __karma__.loaded = function () {
 };
 
-var distPath = '/base/dist/';
-var appPaths = ['app']; //Add all valid source code folders here
 
 function isJsFile(path) {
   return path.slice(-3) == '.js';
 }
 
 function isSpecFile(path) {
-  return path.slice(-8) == '.spec.js';
+  return path.slice(-8) == '_test.js';
 }
 
-function isAppFile(path) {
-  return isJsFile(path) && appPaths.some(function(appPath) {
-    var fullAppPath = distPath + appPath + '/';
-    return path.substr(0, fullAppPath.length) == fullAppPath;
-  });
+function isBuiltFile(path) {
+  var builtPath = '/base/built/';
+  return isJsFile(path) && (path.substr(0, builtPath.length) == builtPath);
 }
 
 var allSpecFiles = Object.keys(window.__karma__.files)
   .filter(isSpecFile)
-  .filter(isAppFile);
+  .filter(isBuiltFile);
 
 // Load our SystemJS configuration.
 System.config({
-  baseURL: distPath
+  baseURL: '/base'
 });
 
-System.import('system-config.js').then(function() {
-  // Load and configure the TestComponentBuilder.
-  return Promise.all([
-    System.import('@angular/core/testing'),
-    System.import('@angular/platform-browser-dynamic/testing')
-  ]).then(function (providers) {
-    var testing = providers[0];
-    var testingBrowser = providers[1];
+System.config(
+{
+  map: {
+    'rxjs': 'node_modules/rxjs',
+    '@angular': 'node_modules/@angular',
+    'app': 'built'
+  },
+  packages: {
+    'app': {
+      main: 'main.js',
+      defaultExtension: 'js'
+    },
+    '@angular/core': {
+      main: 'index.js',
+      defaultExtension: 'js'
+    },
+    '@angular/compiler': {
+      main: 'index.js',
+      defaultExtension: 'js'
+    },
+    '@angular/common': {
+      main: 'index.js',
+      defaultExtension: 'js'
+    },
+    '@angular/platform-browser': {
+      main: 'index.js',
+      defaultExtension: 'js'
+    },
+    '@angular/platform-browser-dynamic': {
+      main: 'index.js',
+      defaultExtension: 'js'
+    },
+    '@angular/router': {
+      main: 'index.js',
+      defaultExtension: 'js'
+    },
+    'rxjs': {
+      defaultExtension: 'js'
+    }
+  }
+});
 
-    testing.setBaseTestProviders(testingBrowser.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
-      testingBrowser.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
-  });
+Promise.all([
+  System.import('@angular/core/testing'),
+  System.import('@angular/platform-browser-dynamic/testing')
+]).then(function (providers) {
+  var testing = providers[0];
+  var testingBrowser = providers[1];
+
+  testing.setBaseTestProviders(testingBrowser.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
+    testingBrowser.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
+
 }).then(function() {
   // Finally, load all spec files.
   // This will run the tests directly.
@@ -54,3 +88,4 @@ System.import('system-config.js').then(function() {
       return System.import(moduleName);
     }));
 }).then(__karma__.start, __karma__.error);
+
